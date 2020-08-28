d0bddb24b3113e6111a1e7d611e9f76ed25f6cf6
hengboy <jnyuqy@gmail.com>
2020-08-28 10:27:52 +0800

    :bug: 修改获取本机IP方法

--- message-pipe-core/src/main/java/org/minbox/framework/message/pipe/core/untis/InternetAddressUtils.java
+++ message-pipe-core/src/main/java/org/minbox/framework/message/pipe/core/untis/InternetAddressUtils.java
@@ -1,20 +1,49 @@
 package org.minbox.framework.message.pipe.core.untis;
 
-import org.minbox.framework.message.pipe.core.exception.MessagePipeException;
-
-import java.net.InetAddress;
+import java.net.*;
+import java.util.Enumeration;
 
 /**
+ * The internet address utils
+ *
  * @author 恒宇少年
  */
 public class InternetAddressUtils {
-
-    public static String getLocalHost() {
+    /**
+     * Obtain the IPv4 address according to the network card
+     *
+     * @return IPv4 address
+     */
+    public static String getLocalIpByNetCard() {
         try {
-            InetAddress inetAddress = InetAddress.getLocalHost();
-            return inetAddress.getHostAddress();
-        } catch (Exception e) {
-            throw new MessagePipeException(e.getMessage(), e);
+            for (Enumeration<NetworkInterface> e = NetworkInterface.getNetworkInterfaces(); e.hasMoreElements(); ) {
+                NetworkInterface item = e.nextElement();
+                for (InterfaceAddress address : item.getInterfaceAddresses()) {
+                    if (item.isLoopback() || !item.isUp()) {
+                        continue;
+                    }
+                    if (address.getAddress() instanceof Inet4Address) {
+                        Inet4Address inet4Address = (Inet4Address) address.getAddress();
+                        return inet4Address.getHostAddress();
+                    }
+                }
+            }
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (SocketException | UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Obtain Ipv4 address according to {@link InetAddress}
+     *
+     * @return Ipv4 address
+     */
+    public static String getLocalIP() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
         }
     }
 }
