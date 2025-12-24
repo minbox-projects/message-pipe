949df9aa7d5e5ae2dbd7c135e4cd756dafc11cca
恒宇少年 <jnyuqy@gmail.com>
2025-12-24 17:51:08 +0800

    Add message serialization support

--- message-pipe-core/src/main/java/org/minbox/framework/message/pipe/core/Message.java
+++ message-pipe-core/src/main/java/org/minbox/framework/message/pipe/core/Message.java
@@ -8,6 +8,7 @@ import java.io.Serializable;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Message entity in the pipeline
@@ -19,15 +20,20 @@ import java.util.Map;
 @ToString
 public class Message implements Serializable {
     private static final String DEFAULT_ENCODING = Charset.defaultCharset().name();
+    /**
+     * The unique identifier of the message
+     */
+    private String messageId;
     private String bodyEncoding;
     private byte[] body;
-    private Map<String, Object> metadata = new HashMap<>();
+    private final Map<String, Object> metadata = new HashMap<>();
 
     public Message(byte[] body) {
         this(body, DEFAULT_ENCODING);
     }
 
     public Message(byte[] body, String bodyEncoding) {
+        this.messageId = UUID.randomUUID().toString();
         this.bodyEncoding = bodyEncoding;
         this.body = body;
     }
