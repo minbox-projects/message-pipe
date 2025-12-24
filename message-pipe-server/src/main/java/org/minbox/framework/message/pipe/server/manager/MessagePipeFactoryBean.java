2d98c50827ef2da5e85dd89f361148fc5cddedcc
恒宇少年 <jnyuqy@gmail.com>
2025-12-24 10:16:03 +0800

    Inject ServiceDiscovery into MessagePipeFactoryBean

--- message-pipe-server/src/main/java/org/minbox/framework/message/pipe/server/manager/MessagePipeFactoryBean.java
+++ message-pipe-server/src/main/java/org/minbox/framework/message/pipe/server/manager/MessagePipeFactoryBean.java
@@ -3,6 +3,7 @@ package org.minbox.framework.message.pipe.server.manager;
 import org.minbox.framework.message.pipe.core.exception.MessagePipeException;
 import org.minbox.framework.message.pipe.server.MessagePipe;
 import org.minbox.framework.message.pipe.server.config.MessagePipeConfiguration;
+import org.minbox.framework.message.pipe.server.service.discovery.ServiceDiscovery;
 import org.redisson.api.RedissonClient;
 
 /**
@@ -19,11 +20,19 @@ public class MessagePipeFactoryBean {
      * The redisson client instance
      */
     private RedissonClient redissonClient;
+    /**
+     * The service discovery instance
+     */
+    private ServiceDiscovery serviceDiscovery;
 
-    public MessagePipeFactoryBean(RedissonClient redissonClient) {
+    public MessagePipeFactoryBean(RedissonClient redissonClient, ServiceDiscovery serviceDiscovery) {
         this.redissonClient = redissonClient;
+        this.serviceDiscovery = serviceDiscovery;
         if (this.redissonClient == null) {
             throw new MessagePipeException("The RedissonClient is must not be null.");
+        }
+        if (this.serviceDiscovery == null) {
+            throw new MessagePipeException("The ServiceDiscovery is must not be null.");
         }
     }
 
@@ -35,6 +44,6 @@ public class MessagePipeFactoryBean {
      * @return {@link MessagePipe} instance
      */
     public MessagePipe createMessagePipe(String name, MessagePipeConfiguration configuration) {
-        return new MessagePipe(name, this.redissonClient, configuration);
+        return new MessagePipe(name, this.redissonClient, configuration, this.serviceDiscovery);
     }
 }
