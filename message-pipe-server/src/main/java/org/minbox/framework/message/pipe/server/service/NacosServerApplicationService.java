9cb7a6827d698234fa17402ea588cfafeb206b89
恒宇少年 <jnyuqy@gmail.com>
2025-12-26 15:26:04 +0800

    Fix Nacos client heartbeat refresh and extract constants

--- message-pipe-server/src/main/java/org/minbox/framework/message/pipe/server/service/NacosServerApplicationService.java
+++ message-pipe-server/src/main/java/org/minbox/framework/message/pipe/server/service/NacosServerApplicationService.java
@@ -7,6 +7,8 @@ import com.alibaba.nacos.api.naming.listener.NamingEvent;
 import com.alibaba.nacos.api.naming.pojo.Instance;
 import org.minbox.framework.message.pipe.core.PipeConstants;
 import org.minbox.framework.message.pipe.core.information.ClientInformation;
+import org.minbox.framework.message.pipe.server.service.ServiceEvent;
+import org.minbox.framework.message.pipe.server.service.ServiceEventType;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationEventPublisher;
@@ -14,6 +16,9 @@ import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.util.ObjectUtils;
 
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -25,8 +30,17 @@ import java.util.stream.Collectors;
  */
 public class NacosServerApplicationService implements InitializingBean, DisposableBean, EventListener,
         ApplicationEventPublisherAware {
-    private NamingService namingService;
+    /**
+     * The heartbeat refresh initial delay
+     */
+    private static final long DEFAULT_HEARTBEAT_REFRESH_INITIAL_DELAY = 5;
+    /**
+     * The heartbeat refresh period
+     */
+    private static final long DEFAULT_HEARTBEAT_REFRESH_PERIOD = 5;
+    private final NamingService namingService;
     private ApplicationEventPublisher applicationEventPublisher;
+    private final ScheduledExecutorService heartbeatRefresher;
 
     /**
      * Constructs a new NacosServerApplicationService instance
@@ -35,6 +49,7 @@ public class NacosServerApplicationService implements InitializingBean, Disposab
      */
     public NacosServerApplicationService(NamingService namingService) {
         this.namingService = namingService;
+        this.heartbeatRefresher = Executors.newScheduledThreadPool(1);
     }
 
     @Override
@@ -65,10 +80,32 @@ public class NacosServerApplicationService implements InitializingBean, Disposab
     @Override
     public void destroy() throws Exception {
         this.namingService.shutDown();
+        if (this.heartbeatRefresher != null) {
+            this.heartbeatRefresher.shutdown();
+        }
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
         this.namingService.subscribe(PipeConstants.CLIENT_SERVICE_NAME, this);
+        // Start heartbeat refresher
+        this.heartbeatRefresher.scheduleAtFixedRate(() -> {
+            try {
+                List<Instance> instances = this.namingService.selectInstances(PipeConstants.CLIENT_SERVICE_NAME, true);
+                if (!ObjectUtils.isEmpty(instances)) {
+                    List<ClientInformation> clients = instances.stream()
+                            .filter(instance -> instance.getMetadata().containsKey(PipeConstants.PIPE_NAMES_METADATA_KEY))
+                            .map(instance -> ClientInformation.valueOf(instance.getIp(), instance.getPort(),
+                                    instance.getMetadata().get(PipeConstants.PIPE_NAMES_METADATA_KEY)))
+                            .collect(Collectors.toList());
+                    if (!ObjectUtils.isEmpty(clients)) {
+                        ServiceEvent serviceEvent = new ServiceEvent(this, ServiceEventType.HEART_BEAT, clients);
+                        applicationEventPublisher.publishEvent(serviceEvent);
+                    }
+                }
+            } catch (Exception e) {
+                // Ignore errors
+            }
+        }, DEFAULT_HEARTBEAT_REFRESH_INITIAL_DELAY, DEFAULT_HEARTBEAT_REFRESH_PERIOD, TimeUnit.SECONDS);
     }
 }
